583d4250f991825380917582e2e62aa9316d9c2b
Marija Akimova <Hallabuda60@gmail.com>
2023-04-04 11:52:17 +0300

    User and Role class changed

--- learningPlatform/src/main/java/com/example/learningPlatform/DrivingLearningPlatformApplication.java
+++ learningPlatform/src/main/java/com/example/learningPlatform/DrivingLearningPlatformApplication.java
@@ -1,5 +1,7 @@
 package com.example.learningPlatform;
 
+import com.example.learningPlatform.model.Role;
+import com.example.learningPlatform.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class DrivingLearningPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrivingLearningPlatformApplication.class, args);
+		User newUser = new User("Jonas",  "Kazlauskas", Role.admin, "jonas@gmail.com", "felix", "Radmin1$");
+		System.out.println(newUser);
 	}
 
 }
--- learningPlatform/src/main/java/com/example/learningPlatform/model/User.java
+++ learningPlatform/src/main/java/com/example/learningPlatform/model/User.java
@@ -8,39 +8,35 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Table(name = "users")
-@Entity
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Setter(value = AccessLevel.NONE)
-    @Column(name = "User_id", updatable = false, nullable = false, unique=true)
+    @Column(name = "id", updatable = false, nullable = false, unique=true)
     private long uid;
-    @Column(name = "User_name")
+    @Column(name = "Name")
     @NotBlank(message = "Not blank")
     @Size(min = 3,max = 30, message = "Out of range min 3 un max 30")
     @Pattern(regexp = "[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*+[- ]{1}+[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*|[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*", message = "examples June or Mary Barbara")
     private String name;
-    @Column(name = "User_surname")
+    @Column(name = "Surname")
     @NotBlank(message = "Not blank")
     @Size(min = 3,max = 30, message = "Out of range min 3 un max 30")
     @Pattern(regexp = "[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*+[- ]{1}+[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*|[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}+[a-zāčēģīķļņšūž]*", message = "examples Alien or Harding-Rolls")
     private String surname;
-    @Column(name = "User_role")
+    @Column(name = "Role")
     @NotBlank(message = "Not blank")
     private Role role = Role.guest;
-    @Column(name = "User_email")
+    @Column(name = "Email")
     @NotBlank(message = "Not blank")
     @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Can use alphabet, numbers and .-")
     private String email;
-    @Column(name = "User_username")
+    @Column(name = "Username")
     @NotBlank(message = "Not blank")
-    @Pattern(regexp = "^[a-zA-Z0-9]{6,16}", message = "From 6 to 16 numbers and alphabet")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,16}", message = "From 6 to 16 numbers and alphabet")
     private String username;
-    @Column(name = "User_password")
+    @Column(name = "Password")
     @NotBlank(message = "Not blank")
     @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")
     private String password;
