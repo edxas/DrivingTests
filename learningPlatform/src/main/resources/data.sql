e57070c26b66ccb9d24966d3e13896aa93de0d28
feliciyus <jonas.kazlauskas6@gmail.com>
2023-04-04 11:07:23 +0300

    schema finished

--- learningPlatform/src/main/resources/data.sql
+++ learningPlatform/src/main/resources/data.sql
@@ -1,5 +1,1 @@
-INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Jonas Kazlauskas', 'jonas@gmail.com', '952512');
-
-INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Vytenis Domkus', 'Vytenis@gmail.com', '9444352512');
-
-INSERT INTO employee (Name, Email, Phone_Number, ru) VALUES ('Vytenis Domkus', 'Vytenis@gmail.com', '944', 'bvbws');
+INSERT INTO users (Role, Name, Surname, Email, Username, Password) VALUES ('admin', 'Jonas', 'Kazlauskas', 'jonas@gmail.com', 'felix', '952512');
