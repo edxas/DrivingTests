104408805a01cd0ed041c05c02f9d127f74f7e45
feliciyus <jonas.kazlauskas6@gmail.com>
2023-04-04 10:26:56 +0300

    adding schema and data files, content inside not correct yet

---
+++ learningPlatform/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Jonas Kazlauskas', 'jonas@gmail.com', '952512');
+
+INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Vytenis Domkus', 'Vytenis@gmail.com', '9444352512');
+
+INSERT INTO employee (Name, Email, Phone_Number, ru) VALUES ('Vytenis Domkus', 'Vytenis@gmail.com', '944', 'bvbws');
